55c9bafc6bf74720701b509b4becbae26a0f3683
JeeDeWee <57176355+JeeDeWee@users.noreply.github.com>
2022-07-15 16:27:23 +0200

    removed output to log file

--- ddnss-update.sh
+++ ddnss-update.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/sh
 
 KEYAUTH=$DDNSS_KEYAUTH
 HOSTNAME=$DDNSS_HOSTNAME
@@ -8,14 +8,14 @@ ALLHOST="all"
 DATE=`date +%Y-%m-%d\ %H:%M:%S`
 IP=`wget -q -O - https://www.ddnss.de/meineip.php| grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
 UPDIP=`cat $CONFIG/updip.txt`
-echo "Current IP=$UPDIP" >> $CONFIG/log.txt
+echo "Current IP=$UPDIP"
 
 if [ "$IP" == "$UPDIP" ]; then
-    echo "$DATE - IP is unchanged - NO UPDATE" >> $CONFIG/log.txt
+    echo "$DATE - IP is unchanged - NO UPDATE"
 else
-    echo "$DATE - New-IP: $ip / Old-IP: $UPDIP - UPDATE!" >> $CONFIG/log.txt
+    echo "$DATE - New-IP: $ip / Old-IP: $UPDIP - UPDATE!"
     echo $IP > $CONFIG/updip.txt
     wget -q -O - 'https://www.ddnss.de/upd.php?key='$KEYAUTH'&host='$HOSTNAME'&host='$ALLHOST''
     echo " "
-    echo "Update of IP address performed." >> $CONFIG/log.txt
+    echo "Update of IP address performed."
 fi
